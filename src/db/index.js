1d923711ff555d636c7a4557c86dd3197cf71483
amey <ameybobade07@gmail.com>
2025-12-18 22:58:22 +0530

    connect to mongoAtlas

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionnInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`\n mongoDB connection !! DB Host: ${connectionnInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
